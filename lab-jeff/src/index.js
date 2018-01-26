5a350b47f3f1994dcd75a30ac94329da19cd85cb
jjkusowski <jjkusowski@yahoo.com>
2018-01-25 16:54:28 -0800

    3-way quicksort

--- lab-jeff/src/index.js
+++ lab-jeff/src/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const heapSort = require('./heap-sort');
-
+const quickSort = require('./quick-sort-3way');
 let array = [5, 10, 6, 12, 14, 18, 7];
 
-heapSort(array);
+quickSort(array);
 console.log(array);
