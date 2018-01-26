2ba87757b236341c681c8b054abe8bb3c698846b
Catherine Looper <loopercat@gmail.com>
2018-01-25 18:10:25 -0800

    Scaffolded stable quick sort algorithm

---
+++ lab-catherine/src/stable-quick-sort.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const quickSort = items => {
+  _quickSortHelper(items, 0, items.length - 1);
+};
+
+const _quickSortHelper = (items, leftIndex, rightIndex) => {
+  if(rightIndex > leftIndex) {
+    let partitionIndex = _partition(items, leftIndex, rightIndex);
+
+    _quickSortHelper(items, leftIndex, partitionIndex - 1);
+    _quickSortHelper(items, partitionIndex + 1, rightIndex);
+  }
+};
+
+const _partition = (items, leftIndex, rightIndex) => {
+  let pivotIndex = rightIndex; 
+  let firstHighIndex = leftIndex;
+
+  for(let i = leftIndex; i < rightIndex; i++) {
+    if(items[i] < items[pivotIndex]) {
+      _swap(items, i, firstHighIndex);
+      firstHighIndex++;
+    }
+  }
+  _swap(items, pivotIndex, firstHighIndex);
+  return firstHighIndex;
+};
+
+const _swap = (items, indexA, indexB) => {
+  let helper = items[indexA];
+  items[indexA] = items[indexB];
+  items[indexB] = helper;
+};
+
+module.exports = quickSort;
