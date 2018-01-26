71c17417419583c44576474ef993d76ca193e31c
Jacob M-G Evans <jacobmgevans@gmail.com>
2018-01-25 22:07:24 -0800

    started working on merge, scaffolding done

---
+++ lab-jacob/src/merge.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const mergeSort = (items) => {
+  let output = [];
+
+  if(items.length < 2)
+    return items;
+  
+  if(items.length === 2)
+    return items[0] < items[1] ? items : items.reverse();
+
+  let middle = Math.floor(items.length / 2);
+  let leftSide = mergeSort(items.slice(0,middle));
+  let rightSide = mergeSort(items.slice(middle));
+
+  while(leftSide.length || rightSide.length){
+    if(leftSide.length && !rightSide.length){
+      return output.push.apply(leftSide);
+      
+    }
+    if(!leftSide.length && rightSide.length){
+      return output.push.apply(rightSide); 
+    
+    }
+    if(leftSide[0] <= rightSide[0])
+      output.push(leftSide.shift());
+    else
+      output.push(rightSide.shift());
+  }
+
+  return output;
+};
+
+module.exports = mergeSort;
+
