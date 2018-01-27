9d547ade5e74a8f34f9f7d91f14975e8e148ba35
Andrew Bloom <blooma116@gmail.com>
2018-01-26 21:34:28 -0800

    refactored recursion as loop, to improve memory usage

--- lab-andrew/src/lib/radix-sort.js
+++ lab-andrew/src/lib/radix-sort.js
@@ -1,28 +1,17 @@
 'use strict';
 
 const radixSort = items => {
-  return _radixSort(items, 1);
-};
-
-const _radixSort = (items, divisor, largestItem = Math.max(...items)) => {
-  // if (largestItem === undefined){
-  //   largestItem = Math.max(...items);
-  // }
-  if (divisor < largestItem){
+  const largestItem = Math.max(...items);
+  let divisor = 1;
+  while (divisor < largestItem){
     const buckets = Array(10).fill(null).map(() => []);
     for (let num of items){
-      const significantDigit = (num % (divisor * 10));
-      const bucketIndex = Math.floor(significantDigit / (divisor));
+      const significantDigit = num % (divisor * 10);
+      const bucketIndex = Math.floor(significantDigit / divisor);
       buckets[bucketIndex].push(num);
     }
-
-    items = [];
-
-    for (let bucket of buckets){
-      items.push(...bucket);
-    }
-
-    return _radixSort(items, divisor * 10, largestItem);
+    items = buckets.reduce(((acc, e) => acc.concat(e)), []);
+    divisor *= 10;
   }
   return items;
 };
