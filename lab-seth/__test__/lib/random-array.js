279695dc9af1a01b01e005f9df8cf9bcba3fab4b
SethDonohue <donohue.seth@gmail.com>
2018-01-28 16:13:18 -0800

    added tests for stabl quicksort across empty, single, small, medium, and large arrays

--- lab-seth/__test__/lib/random-array.js
+++ lab-seth/__test__/lib/random-array.js
@@ -1,16 +1,15 @@
 'use strict'
-const makeRandomArray = () => {
+const makeRandomArray = (arrayLength) => {
 
-  const sign = () =>{
-    return Math.random() > .5 ? -1: 1
+  const plusOrMinus = () =>{
+    return Math.random() < .5 ? -1: 1
   }
 
   let array = [];
-  let arrayLength = Math.floor(Math.random() * 100)
   let elementLength = Math.floor((Math.random() * 10))
-  
+
   for(let i = 0; i < arrayLength; i++){
-    array.push(sign()*(Math.floor(Math.pow(Math.random()*10, elementLength))))
+    array.push(plusOrMinus()*(Math.floor(Math.pow(Math.random()*10, elementLength))))
   }
   return array
 }
