4aac17a4cbeb61fcd0b0fd0939c7b25bdbccf8d6
David Lindahl <davidalindahl@gmail.com>
2018-01-28 21:02:57 -0800

    changed some names, addes some tests

--- lab-david/src/merge-sort.js
+++ lab-david/src/merge-sort.js
@@ -13,10 +13,10 @@ const mergeSort = items => {
   if(items.length === 2)
     return items[0] < items[1] ? items : items.reverse();
 
-  let middle = Math.floor(items.length / 2);
+  let pivot = Math.floor(items.length / 2);
 
-  let leftSide = mergeSort(items.slice(0, middle));
-  let rightSide = mergeSort(items.slice(middle));
+  let leftSide = mergeSort(items.slice(0, pivot));
+  let rightSide = mergeSort(items.slice(pivot));
 
   while(leftSide.length || rightSide.length){
     if(leftSide.length && !rightSide.length){
